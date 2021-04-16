9b37734307d2e2e9b22250198ffad0f3c8aa5ae5
viktor.shslyapin <v.shalyapin3323@gmail.com>
2021-04-16 17:09:50 +0300

    fix(X2))

--- src/Components/BtnComponent.js
+++ src/Components/BtnComponent.js
@@ -4,22 +4,22 @@ function BtnComponent(props) {
   return (
     <div>
       {(props.status === 0)? 
-        <button className="stopwatch-btn stopwatch-btn-gre"
+        <button className="stopwatch-btn"
         onClick={props.start}>Start</button> : ""
       }
 
       {(props.status === 1)? 
         <div>
-          <button className="stopwatch-btn stopwatch-btn-gre"
+          <button className="stopwatch-btn"
                   onClick={props.stop}>Stop</button>
-          <button className="stopwatch-btn stopwatch-btn-gre"
+          <button className="stopwatch-btn"
                   onClick={props.wait}>Wait</button>
         </div> : ""
       }
 
      {(props.status === 2)? 
         <div>
-          <button className="stopwatch-btn stopwatch-btn-gre"
+          <button className="stopwatch-btn"
                   onClick={props.reset}>Reset</button>
         </div> : ""
       }
@@ -27,9 +27,9 @@ function BtnComponent(props) {
       
      {(props.status === 3)? 
         <div>
-          <button className="stopwatch-btn stopwatch-btn-gre"
+          <button className="stopwatch-btn"
                   onClick={props.start}>Start</button>
-          <button className="stopwatch-btn stopwatch-btn-gre"
+          <button className="stopwatch-btn"
                   onClick={props.reset}>Reset</button>
         </div> : ""
       }
